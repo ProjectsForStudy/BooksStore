3649a5fbcb66c96459c75b2bbf9954258dbb6733
Konstantin <kostyakalashnikov607@gmail.com>
2023-05-02 21:41:33 +0200

    first

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import cors from "cors";
 const app = express() 
 
 const db = mysql.createConnection({
-    /* host:"localhost",
+   /*  host:"localhost",
     user:"root",
     password:"12483615Ss.",
-    database:"test" */
-    host:"185.151.245.185",
+    database:"test"   */
+     host:"185.151.245.185",
     user:"Kostya",
     password:"12483615Ss.",
-    database:"test" 
+    database:"test"   
 });
 
 app.use(express.json());
